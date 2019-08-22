346977a6a6d508e89049b53a63e96a66494bd20c
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2019-08-22 14:03:12 +0200

    Add better function definitions / splits

--- src/stores/CorrectAnswers.js
+++ src/stores/CorrectAnswers.js
@@ -19,52 +19,57 @@ multiplayer.subscribe(val => {
 		multiplayerConn.db
 			.collection("games")
 			.doc(multiplayerConn.game_uid)
-			.onSnapshot(doc => {
-				const game = doc.data();
-				if (!game) {
-					return;
-				}
-				const answers = game.answers.map(word => ({
-					data: words_value.find(w => w.uid == word.uid),
-					string: word.string
-				}));
-				correctAnswers.set(answers.map(a => a.data));
-				answers.forEach(answer => {
-					crossword.handleWordCoords(
-						answer.data.coords,
-						answer.string
-					);
-				});
-			});
+			.onSnapshot(doc => addMultiplayerAnswers(doc.data()));
 	}
 });
 
+function addMultiplayerAnswers(game) {
+	if (!game) {
+		return;
+	}
+	const answers = game.answers.map(word => ({
+		data: words_value.find(w => w.uid == word.uid),
+		string: word.string
+	}));
+	correctAnswers.set(answers.map(a => a.data));
+	answers.forEach(answer => {
+		crossword.handleWordCoords(answer.data.coords, answer.string);
+	});
+}
+
 export const correctAnswers = writable([]);
+
+function addOwnCorrectAnswer(activeWord) {
+	correctAnswers.update(answers => {
+		if (answers.some(a => a.uid == activeWord.uid)) {
+			return answers;
+		}
+
+		const newAnswers = [...answers, activeWord];
+		if (multiplayerConn && multiplayerConn.game_uid) {
+			saveAsMultiplayerAnswers(newAnswers);
+		}
+
+		return newAnswers;
+	});
+}
+
+function saveAsMultiplayerAnswers(answers) {
+	const mappedAnswers = answers.map(a => ({
+		uid: a.uid,
+		string: crossword.getAnswerString(a.coords)
+	}));
+	multiplayerConn.db
+		.collection("games")
+		.doc(multiplayerConn.game_uid)
+		.set({
+			answers: mappedAnswers
+		});
+}
 
 activeWord.subscribe(val => {
 	const { isCorrect, activeWord } = validate(val);
 	if (isCorrect) {
-		correctAnswers.update(answers => {
-			if (answers.some(a => a.uid == activeWord.uid)) {
-				return answers;
-			}
-
-			const newAnswers = [...answers, activeWord];
-			const mappedAnswers = newAnswers.map(a => ({
-				uid: a.uid,
-				string: crossword.getAnswerString(a.coords)
-			}));
-
-			if (multiplayerConn) {
-				multiplayerConn.db
-					.collection("games")
-					.doc(multiplayerConn.game_uid)
-					.set({
-						answers: mappedAnswers
-					});
-			}
-
-			return newAnswers;
-		});
+		addOwnCorrectAnswer(activeWord);
 	}
 });
